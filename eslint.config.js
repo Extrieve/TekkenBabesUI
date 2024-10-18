48e4b6d2062472e678c42e3a9433d81aaf28bc95
Extrieve <nicolas.cinera@gmail.com>
2024-10-17 23:21:28 -0400

    chore: Update Prettier and ESLint configurations
    - Update .prettierrc to include a newline at the end of the file
    - Update eslint.config.js to import prettier and disable the rule that requires React in scope
    - Update tailwind.config.js to reformat the code and add missing exports

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,19 @@
 import pluginJs from "@eslint/js";
+import prettier from "eslint-config-prettier";
 import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  prettier,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off", // Disable the rule that requires React in scope
+    },
+  },
 ];
