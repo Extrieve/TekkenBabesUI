fe34be4322b2bf3bf668a531e4395151f78bbfd1
Extrieve <nicolas.cinera@gmail.com>
2024-10-17 22:59:49 -0400

    Initial commit: Set up Vite, React, Tailwind CSS, shadcn/ui with TypeScript using pnpm

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Button variant="default">Click Me</Button>
+    </div>
+  )
+}
+
+export default App
